4085da2bde9241aa864b358c2079aa8dc72993d6
Norton Andreev <norton@nortonandreev.com>
2025-05-27 18:28:00 +0300

    Add Prettier configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,30 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2022": true,
-		"node": true
-	},
-	"extends": [
-		"@dusk-network/eslint-config/js",
-		"@dusk-network/eslint-config/svelte"
-	],
-	"ignorePatterns": [
-		"/*.*",
-		"/**/__tests__/**",
-		"/**/__mocks__/**"
-	],
-	"root": true,
-	"settings": {
-		"import/resolver": {
-			"eslint-import-resolver-custom-alias": {
-				"alias": {
-					"$app": "node_modules/@sveltejs/kit/src/runtime/app",
-					"$config": "./src/config",
-					"$lib": "./src/lib",
-					"@sveltejs/kit": "node_modules/@sveltejs/kit/src/exports/index.js",
-					"@testing-library/svelte": "node_modules/@testing-library/svelte/src/index.js",
-					"svelte/motion": "node_modules/svelte/src/runtime/motion/index.js",
-					"svelte/store": "node_modules/svelte/src/runtime/store/index.js"
-				},
-				"extensions": [".cjs", ".js", ".json", ".svelte"]
-			}
-		}
-	}
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    "@dusk-network/eslint-config/js",
+    "@dusk-network/eslint-config/svelte",
+  ],
+  ignorePatterns: ["/*.*", "/**/__tests__/**", "/**/__mocks__/**"],
+  root: true,
+  settings: {
+    "import/resolver": {
+      "eslint-import-resolver-custom-alias": {
+        alias: {
+          $app: "node_modules/@sveltejs/kit/src/runtime/app",
+          $config: "./src/config",
+          $lib: "./src/lib",
+          "@sveltejs/kit": "node_modules/@sveltejs/kit/src/exports/index.js",
+          "@testing-library/svelte":
+            "node_modules/@testing-library/svelte/src/index.js",
+          "svelte/motion": "node_modules/svelte/src/runtime/motion/index.js",
+          "svelte/store": "node_modules/svelte/src/runtime/store/index.js",
+        },
+        extensions: [".cjs", ".js", ".json", ".svelte"],
+      },
+    },
+  },
 };
